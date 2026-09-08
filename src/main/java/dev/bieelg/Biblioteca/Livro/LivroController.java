7f778a98df6dafc22030a147dd7095879abfcf94
bieelg18 <gabriel_mjj.alves@hotmail.com>
2026-09-08 12:57:48 -0300

    feat: Crias as controllers ainda sem os endpoints

---
+++ src/main/java/dev/bieelg/Biblioteca/Livro/LivroController.java
@@ -0,0 +1,9 @@
+package dev.bieelg.Biblioteca.Livro;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/livro")
+public class LivroController {
+}
---
+++ src/main/java/dev/bieelg/Biblioteca/Usuario/UsuarioController.java
@@ -0,0 +1,9 @@
+package dev.bieelg.Biblioteca.Usuario;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+}
