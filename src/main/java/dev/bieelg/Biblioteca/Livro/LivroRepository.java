c0529f58f16ea8bcc1d40c63dcc73f1eade63a14
bieelg18 <gabriel_mjj.alves@hotmail.com>
2026-09-09 12:21:37 -0300

    feat: Cria um novo método no LivroRepository

--- src/main/java/dev/bieelg/Biblioteca/Livro/LivroRepository.java
+++ src/main/java/dev/bieelg/Biblioteca/Livro/LivroRepository.java
@@ -2,10 +2,13 @@ package dev.bieelg.Biblioteca.Livro;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LivroRepository extends JpaRepository<Livro, Integer> {
 
     Optional<Livro> findByLivro(String nome);
+
+    List<Livro> findByStatus(StatusLivro status);
 
 }
